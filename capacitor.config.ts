f57b000331530b53455da7940a1626cf4a3bb040
Kayode Bristol <kayode.bristol@gmail.com>
2022-02-09 14:20:53 -0800

    capacitor & electron added

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.project.tracker.svelte',
+  appName: 'svelte-markdown-project-tracker',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
